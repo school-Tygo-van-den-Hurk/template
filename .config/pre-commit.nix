95480d66b6fb267a75c6da17d395df75f5c92d71
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-05-10 22:12:27 +0200

    feat: more pre commit checks (#7)
    - Added pre-commit enforced branch protection.
    - Added a lot of new pre-commit checks
    
    The following checks were added:
    - name: flake-checker
      description: Run health checks on your flake-powered Nix projects.
    - name: check-shebang-scripts-are-executable
      description: Ensure that all (non-binary) files with a shebang are executable
    - name: detect-private-keys
      description: Detect the presence of private keys.
    - name: check-symlinks
      description: Checks for broken symlinks in the repository.
    - name: gitlint
      description: Linting for your git commit messages
    
    The following checks were not added:
    - name: lychee
      description: Checks for broken links in Markdown files.
      because: (is broken) checking links is not possible in the sandbox.
    - name: editorconfig-checker
      description: Verify that the files are in harmony with the  .editorconfig.
      because: (is clunky) might cause formatters to fight

--- .config/pre-commit.nix
+++ .config/pre-commit.nix
@@ -1,6 +1,16 @@
+# See https://github.com/cachix/git-hooks.nix/blob/fa466640195d38ec97cf0493d6d6882bc4d14969/modules/hooks.nix
 {
   src = ./..;
   hooks = {
+
+    # Run health checks on your flake-powered Nix projects.
+    flake-checker = {
+      enable = true;
+      stages = [
+        "pre-push"
+        "manual"
+      ];
+    };
 
     # Check if the flake passes all it's checks. (takes a long time)
     nix-flake-check = {
@@ -11,6 +21,44 @@
       stages = [
         "pre-push"
         "manual"
+      ];
+    };
+
+    # Ensure that all (non-binary) files with a shebang are executable.
+    check-shebang-scripts-are-executable = {
+      enable = true;
+      stages = [
+        "pre-commit"
+        "pre-push"
+        "manual"
+      ];
+    };
+
+    # Detect the presence of private keys.
+    detect-private-keys = {
+      enable = true;
+      stages = [
+        "pre-commit"
+        "pre-push"
+        "manual"
+      ];
+    };
+
+    # Checks for broken symlinks in the repository.
+    check-symlinks = {
+      enable = true;
+      stages = [
+        "pre-commit"
+        "pre-push"
+        "manual"
+      ];
+    };
+
+    # Linting for your git commit messages"
+    gitlint = {
+      enable = true;
+      stages = [
+        "commit-msg"
       ];
     };
 
@@ -77,5 +125,26 @@
       ];
     };
 
+    #! Requires an internet connection while in sandbox, thus does not work.
+    # # Checks for broken links in Markdown files.
+    # lychee = {
+    #   enable = true;
+    #   stages = [
+    #     "pre-commit"
+    #     "pre-push"
+    #     "manual"
+    #   ];
+    # };
+
+    #! Does not work properly, especially because formatters fight.
+    # # Verify that the files are in harmony with the `.editorconfig`.
+    # editorconfig-checker = {
+    #   enable = true;
+    #   stages = [
+    #     "pre-commit"
+    #     "pre-push"
+    #     "manual"
+    #   ];
+    # };
   };
 }
