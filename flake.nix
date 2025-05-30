a86ace4b73740f7d4fb9c20f55c3f9735e063e21
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-05-30 14:03:44 +0200

    Moved from 'flake-utils' to 'flake-parts'
    Closes #30: Use flake part instead of flake utils.

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     pre-commit-hooks = {
       url = "github:cachix/git-hooks.nix";
@@ -22,88 +22,97 @@
 
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      flake-utils,
-      treefmt-nix,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
 
-        pkgs = import nixpkgs { inherit system; };
-        treefmtEval = treefmt-nix.lib.evalModule pkgs ./.config/treefmt.nix;
-        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run (import ./.config/pre-commit.nix);
+      imports = [ ];
 
-      in
-      rec {
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Nix Develop ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-        devShells.default = pkgs.mkShell {
-          inherit (pre-commit-check) shellHook;
-          buildInputs =
-            pre-commit-check.enabledPackages
-            ++ (with pkgs; [
-              act # Run / check GitHub Actions locally.
-              git # Pull, commit, and push changes.
-            ]);
+      flake = { };
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+
+          inherit (inputs) treefmt-nix pre-commit-hooks;
+
+          treefmtEval = treefmt-nix.lib.evalModule pkgs ./.config/treefmt.nix;
+          pre-commit-check = pre-commit-hooks.lib.${system}.run (import ./.config/pre-commit.nix);
+
+        in
+        rec {
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Nix Flake Check ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+          checks = packages // {
+            formatting = treefmtEval.config.build.check inputs.self;
+            inherit pre-commit-check;
+          };
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Nix Fmt ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+          formatter = treefmtEval.config.build.wrapper;
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Nix Develop ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+          devShells.default = pkgs.mkShell {
+            inherit (pre-commit-check) shellHook;
+            buildInputs =
+              pre-commit-check.enabledPackages
+              ++ (with pkgs; [
+                act # Run / check GitHub Actions locally.
+                git # Pull, commit, and push changes.
+              ]);
+          };
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Nix Build ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+          packages.default = pkgs.stdenv.mkDerivation rec {
+            name = "default";
+            src = ./.;
+            buildInputs = with pkgs; [ ];
+
+            buildPhase = ''
+              runHook preBuild
+
+              # ...
+
+              runHook postBuild
+            '';
+
+            checkPhase = ''
+              runHook preCheck
+
+              # ...
+
+              runHook postCheck
+            '';
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir --parents $out
+              # ...
+
+              runHook postInstall
+            '';
+          };
+
+          # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
         };
-
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Nix Build ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-
-        packages.default = pkgs.stdenv.mkDerivation rec {
-          name = "default";
-          src = ./.;
-          buildInputs = with pkgs; [ ];
-
-          buildPhase = ''
-            runHook preBuild
-
-            # ...
-
-            runHook postBuild
-          '';
-
-          checkPhase = ''
-            runHook preCheck
-
-            # ...
-
-            runHook postCheck
-          '';
-
-          installPhase = ''
-            runHook preInstall
-
-            mkdir --parents $out
-            # ...
-
-            runHook postInstall
-          '';
-        };
-
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Nix Flake Check ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-
-        checks = packages // {
-          formatting = treefmtEval.config.build.check self;
-          inherit pre-commit-check;
-        };
-
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Nix Fmt ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-
-        formatter = treefmtEval.config.build.wrapper;
-
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-
-      }
-    );
+    };
 }
